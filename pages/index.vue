2c241714a6b0af03fa00019be9fc3921cb942db1
Vladimir Golub <golubvlauri@gmail.com>
2019-11-04 11:46:57 +0200

    add post to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
-
-  </div>
+  <b-container class="main-page">
+    <b-row>
+      <b-col cols="12">
+        <title-element label="1">{{ title[lang] }}</title-element>
+        <posts-list
+          :lang="lang"
+          :posts="posts"
+        />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+  import titleElement from '~/components/elements/titleElement';
+  import postsList from '~/components/common/postsList';
 
-export default {
-
-}
+  export default {
+    data: function () {
+      return {
+        title: {
+          ru: 'Главная страница',
+          en: 'Main page'
+        }
+      }
+    },
+    components: {
+      titleElement,
+      postsList
+    },
+    computed: {
+      posts() {
+        return this.$store.state.posts
+      },
+      lang() {
+        return this.$store.state.lang
+      }
+    }
+  }
 </script>
 
 <style>
