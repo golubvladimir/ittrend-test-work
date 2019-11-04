c3c0e7ad26394373e18ab603964abfc172f7553f
Vladimir Golub <golubvlauri@gmail.com>
2019-11-04 11:58:36 +0200

    add lang to menu

--- components/layout/headerElement.vue
+++ components/layout/headerElement.vue
@@ -4,9 +4,14 @@
       <b-navbar-brand tag="h1" class="mb-0">Blog on Nuxt.js</b-navbar-brand>
     </nuxt-link>
     <b-navbar-nav>
-      <nuxt-link class='nav-link' to="/posts">Блог</nuxt-link>
-      <nuxt-link class='nav-link' to="/about">Об авторе</nuxt-link>
-      <nuxt-link class='nav-link' to="/contacts">Контакты</nuxt-link>
+      <nuxt-link
+        v-for="link in links"
+        :key="link.id"
+        class='nav-link'
+        :to="link.href"
+      >
+        {{ link.title[lang] }}
+      </nuxt-link>
       <select style="width: 50px" @change="changeLang">
         <option selected value="ru">Ru</option>
         <option value="en">En</option>
@@ -18,6 +23,41 @@
 <script>
   export default {
     name: "headerElement",
+    data: function () {
+      return {
+        links: [
+          {
+            id: '1',
+            title: {
+              ru: 'Блог',
+              en: 'Blog'
+            },
+            href: '/posts'
+          },
+          {
+            id: '2',
+            title: {
+              ru: 'Об авторе',
+              en: 'About'
+            },
+            href: '/about'
+          },
+          {
+            id: '3',
+            title: {
+              ru: 'Контакты',
+              en: 'Contacts'
+            },
+            href: '/contacts'
+          },
+        ]
+      }
+    },
+    computed: {
+      lang() {
+        return this.$store.state.lang
+      }
+    },
     methods: {
       changeLang(event) {
         this.$store.dispatch('changeLang', {lang: event.target.value});
